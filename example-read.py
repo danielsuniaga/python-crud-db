1118368214747c8ab34c64a2242727fe404f585d
danielsuniaga <105082657+danielsuniaga@users.noreply.github.com>
2023-08-30 10:48:18 -0500

    Update example-read.py

--- example-read.py
+++ example-read.py
@@ -13,5 +13,3 @@ print(productos)
 miConexion.commit()
 
 miConexion.close()#Cerrar conexion
-
-#LECTURA: https://www.youtube.com/watch?v=m_FzVf9JTV8&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=58
